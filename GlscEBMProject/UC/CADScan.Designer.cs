cbf96315c4fbf00b5948e248c54d51145ebfae75
lamber <zzming123@126.com>
2020-04-07 10:23:57 +0800

    第一次提交，桂林实创

---
+++ GlscEBMProject/UC/CADScan.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace GlscEBMProject.UC
+{
+    partial class CADScan
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupPanel1 = new DevComponents.DotNetBar.Controls.GroupPanel();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCADspeed = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCADBeamVal = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCADfocusOffset = new System.Windows.Forms.TextBox();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtoffset = new System.Windows.Forms.TextBox();
+            this.txtKp = new System.Windows.Forms.TextBox();
+            this.txtScanTime = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBoxCadInfo = new System.Windows.Forms.ListView();
+            this.事件 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(274, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(136, 23);
+            this.txtFileName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.ReadOnly = true;
+            this.txtFileName.Size = new System.Drawing.Size(119, 21);
+            this.txtFileName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "文件路径:";
+            // 
+            // groupPanel1
+            // 
+            this.groupPanel1.CanvasColor = System.Drawing.SystemColors.Control;
+            this.groupPanel1.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
+            this.groupPanel1.DisabledBackColor = System.Drawing.Color.Empty;
+            this.groupPanel1.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.groupPanel1.Location = new System.Drawing.Point(67, 199);
+            this.groupPanel1.Name = "groupPanel1";
+            this.groupPanel1.Size = new System.Drawing.Size(397, 225);
+            // 
+            // 
+            // 
+            this.groupPanel1.Style.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
+            this.groupPanel1.Style.BackColorGradientAngle = 90;
+            this.groupPanel1.Style.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+            this.groupPanel1.Style.BorderBottom = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.groupPanel1.Style.BorderBottomWidth = 1;
+            this.groupPanel1.Style.BorderColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.groupPanel1.Style.BorderLeft = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.groupPanel1.Style.BorderLeftWidth = 1;
+            this.groupPanel1.Style.BorderRight = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.groupPanel1.Style.BorderRightWidth = 1;
+            this.groupPanel1.Style.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.groupPanel1.Style.BorderTopWidth = 1;
+            this.groupPanel1.Style.CornerDiameter = 4;
+            this.groupPanel1.Style.CornerType = DevComponents.DotNetBar.eCornerType.Rounded;
+            this.groupPanel1.Style.TextAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Center;
+            this.groupPanel1.Style.TextColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
+            this.groupPanel1.Style.TextLineAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Near;
+            // 
+            // 
+            // 
+            this.groupPanel1.StyleMouseDown.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            // 
+            // 
+            // 
+            this.groupPanel1.StyleMouseOver.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.groupPanel1.TabIndex = 3;
+            this.groupPanel1.Text = "文件信息";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(136, 57);
+            this.txtFilePath.Margin = new System.Windows.Forms.Padding(2);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.ReadOnly = true;
+            this.txtFilePath.Size = new System.Drawing.Size(119, 21);
+            this.txtFilePath.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 22);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "speed:";
+            // 
+            // txtCADspeed
+            // 
+            this.txtCADspeed.Location = new System.Drawing.Point(73, 19);
+            this.txtCADspeed.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCADspeed.Name = "txtCADspeed";
+            this.txtCADspeed.Size = new System.Drawing.Size(79, 21);
+            this.txtCADspeed.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 47);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "BeamVal:";
+            // 
+            // txtCADBeamVal
+            // 
+            this.txtCADBeamVal.Location = new System.Drawing.Point(73, 44);
+            this.txtCADBeamVal.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCADBeamVal.Name = "txtCADBeamVal";
+            this.txtCADBeamVal.Size = new System.Drawing.Size(79, 21);
+            this.txtCADBeamVal.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(163, 22);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "focusOffset:";
+            // 
+            // txtCADfocusOffset
+            // 
+            this.txtCADfocusOffset.Location = new System.Drawing.Point(247, 19);
+            this.txtCADfocusOffset.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCADfocusOffset.Name = "txtCADfocusOffset";
+            this.txtCADfocusOffset.Size = new System.Drawing.Size(68, 21);
+            this.txtCADfocusOffset.TabIndex = 8;
+            // 
+            // btnApply
+            // 
+            this.btnApply.Location = new System.Drawing.Point(130, 126);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(71, 32);
+            this.btnApply.TabIndex = 10;
+            this.btnApply.Text = "参数应用";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.txtScanTime);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtCADfocusOffset);
+            this.groupBox1.Controls.Add(this.btnApply);
+            this.groupBox1.Controls.Add(this.txtCADspeed);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtCADBeamVal);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(42, 94);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(330, 164);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CAD参数设置";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.txtoffset);
+            this.groupBox2.Controls.Add(this.txtKp);
+            this.groupBox2.Location = new System.Drawing.Point(12, 70);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(303, 50);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "cad文件bit转化";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(152, 25);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 12);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "offset:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 22);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 12);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Kp:";
+            // 
+            // txtoffset
+            // 
+            this.txtoffset.Location = new System.Drawing.Point(218, 22);
+            this.txtoffset.Margin = new System.Windows.Forms.Padding(2);
+            this.txtoffset.Name = "txtoffset";
+            this.txtoffset.Size = new System.Drawing.Size(68, 21);
+            this.txtoffset.TabIndex = 15;
+            this.txtoffset.Text = "1";
+            // 
+            // txtKp
+            // 
+            this.txtKp.Location = new System.Drawing.Point(53, 19);
+            this.txtKp.Margin = new System.Windows.Forms.Padding(2);
+            this.txtKp.Name = "txtKp";
+            this.txtKp.Size = new System.Drawing.Size(79, 21);
+            this.txtKp.TabIndex = 13;
+            // 
+            // txtScanTime
+            // 
+            this.txtScanTime.Location = new System.Drawing.Point(247, 47);
+            this.txtScanTime.Margin = new System.Windows.Forms.Padding(2);
+            this.txtScanTime.Name = "txtScanTime";
+            this.txtScanTime.Size = new System.Drawing.Size(68, 21);
+            this.txtScanTime.TabIndex = 11;
+            this.txtScanTime.Text = "1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(190, 50);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Count";
+            // 
+            // listBoxCadInfo
+            // 
+            this.listBoxCadInfo.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.事件});
+            this.listBoxCadInfo.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listBoxCadInfo.GridLines = true;
+            this.listBoxCadInfo.Location = new System.Drawing.Point(0, 276);
+            this.listBoxCadInfo.Name = "listBoxCadInfo";
+            this.listBoxCadInfo.Size = new System.Drawing.Size(450, 104);
+            this.listBoxCadInfo.TabIndex = 12;
+            this.listBoxCadInfo.UseCompatibleStateImageBehavior = false;
+            this.listBoxCadInfo.View = System.Windows.Forms.View.Details;
+            // 
+            // 事件
+            // 
+            this.事件.Text = "事件";
+            this.事件.Width = 420;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(63, 26);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 12);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "文件名称:";
+            // 
+            // CADScan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.listBoxCadInfo);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "CADScan";
+            this.Size = new System.Drawing.Size(450, 380);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label label1;
+        private DevComponents.DotNetBar.Controls.GroupPanel groupPanel1;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCADspeed;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCADBeamVal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCADfocusOffset;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtScanTime;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtoffset;
+        private System.Windows.Forms.TextBox txtKp;
+        private System.Windows.Forms.ListView listBoxCadInfo;
+        private System.Windows.Forms.ColumnHeader 事件;
+        private System.Windows.Forms.Label label8;
+    }
+}
